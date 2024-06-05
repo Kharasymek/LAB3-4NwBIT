944b06db120b71407621e384f6540551ea8539df
Krystian Harasymek <kharasymek@gmail.com>
2024-06-06 00:57:55 +0200

    Dodano obsługę pojedynczych flag oraz zaktualizowano help

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ "$1" == "--date" ]; then
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
     date
 fi
-if [ "$1" == "--logs" ]; then
+if [[ "$1" == "--logs" || "$1" == "-l" ]]; then
     count=${2:-100}
     for i in $(seq 1 $count); do
         echo "log${i}.txt" > log${i}.txt
@@ -10,10 +10,10 @@ if [ "$1" == "--logs" ]; then
         date >> log${i}.txt
     done
 fi
-if [ "$1" == "--help" ]; then
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
     echo "Dostępne opcje:"
-    echo "--date  : wyświetla dzisiejszą datę"
-    echo "--logs  : tworzy 100 plików logx.txt"
-    echo "--logs N: tworzy N plików logx.txt"
-    echo "--help  : wyświetla dostępne opcje"
+    echo "--date, -d  : wyświetla dzisiejszą datę"
+    echo "--logs, -l  : tworzy 100 plików logx.txt"
+    echo "--logs N, -l N: tworzy N plików logx.txt"
+    echo "--help, -h  : wyświetla dostępne opcje"
 fi
